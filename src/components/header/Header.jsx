efc1863df172e5e34feea095eb4f4653480ff9b1
rabby-shek <rabbyshek1998@gmail.com>
2024-09-16 23:54:01 +0600

    bootstrap cdn links added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
-
+import React from "react";
+import BrandLogo from "../../assets/images/Logo.png";
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="header">
+      <div className="logo-container">
+        <img src={BrandLogo} alt="Logo" />
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a className="nav-link" href="/home">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/about">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/how-its-work">
+              How Its Work
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/services">
+              Services
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="nav-button-container">
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
