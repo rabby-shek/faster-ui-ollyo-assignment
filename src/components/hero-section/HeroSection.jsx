aedd51e51faa405dc99eb44920a5a53ae5e5dd6c
rabby-shek <rabbyshek1998@gmail.com>
2024-09-17 23:49:05 +0600

    hero section styles updated

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <section className='hero-section text-center d-flex flex-column align-items-center justify-content-center'>
-      <div className="hero-section-text mb-3">
+      <div className="hero-section-text">
         Awesome UI Dark Template for Webflow Agency
       </div>
-      <button className="btn btn-dark text-white">Get in Touch</button>
+      <button className="btn hero-section-btn">Get in Touch</button>
     </section>
   );
 }
