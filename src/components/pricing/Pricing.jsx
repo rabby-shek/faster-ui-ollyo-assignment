60118e7cec606e4fbc14da02eec01cb65a327296
rabby-shek <rabbyshek1998@gmail.com>
2024-09-17 22:54:30 +0600

    pricing section structure created

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
 const Pricing = () => {
   return (
-    <div>
-      Pricing
-    </div>
-  )
-}
+    <section className="container pricing">
+      <div className="row">
+        <div className="col-lg-5">
+          <div className="section-title">Pricing</div>
+          <p>
+            Take a look at some of our recent projects to see how we've helped
+            businesses like yours succeed online.
+          </p>
+          <div>
+            <div className="pricing-title">Let’s Schedule a Meeting</div>
+            <button className="btn btn-primary w-100">Schedule Meeting</button>
+          </div>
+        </div>
+        <div className="col-lg-7">
+          <div>Unlimited Services</div>
+          <p>
+            Take a look at some of our recent projects to see how we've helped
+            businesses like yours succeed online.
+          </p>
+          <div className="row">
+            <div className="col-lg-6">
+              <ul>
+                <li>ultimate request</li>
+              </ul>
+            </div>
+            <div className="col-lg-6">
+              <ul>
+                <li>ultimate request</li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">$3,250 /mo</div>
+            <div className="col-6">
+              <FaArrowRightLong />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Pricing;
